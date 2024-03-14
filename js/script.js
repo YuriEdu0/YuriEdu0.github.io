adccb9d2d944f7339a12d1116b025ee1fc79aafa
YuriEdu0 <yurieduardo2704@gmail.com>
2024-03-14 16:40:45 -0300

    tudubao

--- js/script.js
+++ js/script.js
@@ -23,13 +23,14 @@ function validar() {
         return false;
     }
 
-    const anoNascimento = parseInt(ano_nasc.value);
-
-    if (isNaN(anoNascimento)) {
-        alert("Erro: ano de nascimento inválido!");
+    // Verificar se o valor inserido não é um número
+    if (isNaN(ano_nasc.value)) {
+        alert("Erro: por favor, insira um número válido no campo de ano de nascimento!");
         ano_nasc.focus(); // Definindo o foco no campo ano_nasc
         return false;
     }
+
+    const anoNascimento = parseInt(ano_nasc.value);
     
     if (anoNascimento > 2009) {  
         alert("Erro: ano de nascimento inválido!");
@@ -46,6 +47,11 @@ function validar() {
     if (!matutino.checked && !vespertino.checked && !noturno.checked) {
         alert("Erro: nenhum turno selecionado!");
         matutino.focus(); // Definindo o foco no primeiro turno (pode ser ajustado conforme necessário)
+        document.getElementById("avisos").innerHTML = "Campo sobrenome não pode ficar em branco!"; 
+        return false;
+    }
+    else {
+        document.getElementById("avisos").innerHTML = "Seu Nome: " + nome.value + ", Sobrenome: " + sobrenome.value + ", Ano de Nascimento: " + ano_nasc.value + ", Profissão: " + profissao.value; 
         return false;
     }
 
